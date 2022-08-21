984376b5a16f72b6f5e0afee43663dbeb37d60a5
Yandrey Hernández Gracia <33462953+yandrey1185@users.noreply.github.com>
2022-08-21 14:47:49 -0400

    second commit

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ export const GifGridItem = ({ id, title, url }) => {
     return (
         <div className="grid content-center mb-10 mr-10 overflow-hidden rounded-lg border-2 border-rose-500">
             <img className="w-full max-h-44 text-center" alt={ title } src={ url } />
-            <p className="p-5 text-center">{ title }</p>
+            <p className="p-5 text-center"> { title } </p>
         </div>
     );
 };
